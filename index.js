0c8673dd2f5cba2e8c9aa695e0ec851d61cda08b
Dilyana Musievska <dilyana.musievska@gmail.com>
2021-05-18 14:41:04 +0300

    middlewares

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true },
     () => {
     console.log('Connected to MongoDB');
+    });
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+    res.send('Homepage')
 });
 
 app.listen(8800, () => {
