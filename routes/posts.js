2b235b725e52d119cedbb36673a858a98449e4cb
Dilyana Musievska <dilyana.musievska@gmail.com>
2021-05-20 14:24:33 +0300

    async fetch

--- routes/posts.js
+++ routes/posts.js
@@ -79,4 +79,20 @@ router.get('/timeline/all', async (req, res) => {
     }
 });
 
+router.get('/timeline', async (req, res) => {
+    let postsArray = [];
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friend) => {
+               return Post.find({ userId: friendId });
+            })
+        );
+        res.json(userPosts.concat(...friendPosts));
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
 module.exports = router;
