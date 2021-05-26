c0219b9a09174c3c5ead64b2ffec055b54028efa
Dilyana Musievska <dilyana.musievska@gmail.com>
2021-05-26 14:26:52 +0300

    change topbar

--- client/src/components/topbar/Topbar.js
+++ client/src/components/topbar/Topbar.js
@@ -10,28 +10,30 @@ const Topbar = () => {
             </div>
             <div className='topbarCenter'>
                 <div className='searchBar'>
-                    <Search />
-                    <input placeholder='Search' className='searchInput' />
+                    <Search classname='searchIcon' />
+                    <input placeholder='Search in Social App' className='searchInput' />
                 </div>
             </div>
-            <div className='topbarRight'>
-                <span className='topbarLink'>Homepage</span>
-                <span className='topbarLink'>Timeline</span>
-            </div>
-            <div className='topbarIcons'>
-                <div className='topbarIconsItem'>
-                    <Person />
-                    <span className='topbarIconBadge'>1</span>
+            < div className='topbarRight'>
+                <div className='topbarLinks'>
+                    <span className='topbarLink'>Homepage</span>
+                    <span className='topbarLink'>Timeline</span>
+                </div>
+                <div className='topbarIcons'>
+                    <div className='topbarIconsItem'>
+                        <Person />
+                        <span className='topbarIconBadge'>1</span>
+                        </div>
                     <div className='topbarIconsItem'>
                         <Chat />
                         <span className='topbarIconBadge'>2</span>
-                        <div className='topbarIconsItem'>
-                            <Notifications />
-                            <span className='topbarIconBadge'>3</span>
-                        </div>
+                    </div>
+                    <div className='topbarIconsItem'>
+                        <Notifications />
+                        <span className='topbarIconBadge'>3</span>
                     </div>
                 </div>
-                <img src='/images/persons/terry.jpg' alt='' className='topbarImage'/>
+                <img src='/images/persons/terry.jpg' alt='' className='topbarImage' />
             </div>
         </div>
     );
