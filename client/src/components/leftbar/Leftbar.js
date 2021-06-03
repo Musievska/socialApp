6beb0bd106561cfac47595fcfd2d72ad95b90c94
Dilyana Musievska <dilyana.musievska@gmail.com>
2021-06-03 15:21:22 +0300

    leftbar component editing

---
+++ client/src/components/leftbar/Leftbar.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import './leftbar.css';
+import {
+    RssFeed,
+    Chat,
+    PlayCircleFilledOutlined,
+    Group,
+    Bookmark,
+    HelpOutline,
+    WorkOffOutlined,
+    Event,
+    School
+} from '@material-ui/icons';
+
+function Leftbar() {
+    return (
+        <div className='leftbar'>
+            <div className='leftbarWrapper'>
+                <ul className='leftbarList'>
+                    <li className='leftbarListItem'>
+                        <RssFeed className='leftbarIcons' />
+                        <span className='leftbarListItemText'>
+                            Feed
+                        </span>
+                    </li>
+                    <li className='leftbarListItem'>
+                        <Chat className='leftbarIcons' />
+                        <span className='leftbarListItemText'>
+                            Chats
+                        </span>
+                    </li>
+                    <li className='leftbarListItem'>
+                        <PlayCircleFilledOutlined className='leftbarIcons' />
+                        <span className='leftbarListItemText'>
+                            Videos
+                        </span>
+                    </li>
+                    <li className='leftbarListItem'>
+                        <Group className='leftbarIcons' />
+                        <span className='leftbarListItemText'>
+                            Groups
+                        </span>
+                    </li>
+                    <li className='leftbarListItem'>
+                        <Bookmark className='leftbarIcons' />
+                        <span className='leftbarListItemText'>
+                            Bookmarks
+                        </span>
+                    </li>
+                    <li className='leftbarListItem'>
+                        <HelpOutline className='leftbarIcons' />
+                        <span className='leftbarListItemText'>
+                            Questions
+                        </span>
+                    </li>
+                    <li className='leftbarListItem'>
+                        <WorkOffOutlined className='leftbarIcons' />
+                        <span className='leftbarListItemText'>
+                            Jobs
+                        </span>
+                    </li>
+                    <li className='leftbarListItem'>
+                        <Event className='leftbarIcons' />
+                        <span className='leftbarListItemText'>
+                            Events
+                        </span>
+                    </li>
+                    <li className='leftbarListItem'>
+                        <School className='leftbarIcons' />
+                        <span className='leftbarListItemText'>
+                            Courses
+                        </span>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Leftbar;
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Topbar from '../../components/topbar/Topbar';
+import Leftbar from '../../components/leftbar/Leftbar';
+import Feed from '../../components/feed/Feed';
+import Rightbar from '../../components/rightbar/Rightbar';
+import './home.css'
 
 const Home = () => {
     return (
-       <Topbar/>
-    )
+        <>
+            <Topbar />
+            <div className='homeContainer'>
+                <Leftbar />
+                <Feed />
+                <Rightbar />
+            </div>
+        </>
+    );
 }
 
 export default Home;
