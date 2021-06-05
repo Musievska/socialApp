451f1fd4b2e6f274d14e0d57997d42c755c1300a
Dilyana Musievska <dilyana.musievska@gmail.com>
2021-06-05 10:52:29 +0300

    css sharePost

--- client/src/components/sharePosts/SharePosts.js
+++ client/src/components/sharePosts/SharePosts.js
@@ -1,13 +1,42 @@
 import './sharePosts.css';
+import {
+    PermMedia,
+    Label,
+    Room,
+    EmojiEmotions,
+} from '@material-ui/icons'
 
 const SharePosts = () => {
     return (
         <div className='share'>
             <div className='shareWrapper'>
                 <div className='shareTop'>
-                    <div className='shareButton'>
-                        
+                    <img className='shareProfileImg' src='' alt='' />
+                    <input placeholder='Share' className='shareInput'>
+
+                    </input>
+                </div>
+                <hr className='shareHr' />
+                <div className='shareBottom'>
+                    <div className='shareOptions'>
+                        <div className='shareSingleOption'>
+                            <PermMedia htmlColor='tomato' classname='shareIcon' />
+                            <span className='shareOptionContext'>photo or video</span>
+                        </div>
+                        <div className='shareSingleOption'>
+                            <Label htmlColor='blue' classname='shareIcon' />
+                            <span className="shareOptionContext">Tag</span>
+                        </div>
+                        <div className='shareSingleOption'>
+                            <Room htmlColor='green' className='shareIcon' />
+                            <span className='shareOptionContext'>Location</span>
+                        </div>
+                        <div className='shareSingleOption'>
+                            <EmojiEmotions htmlColor='goldenrod' className='shareIcon' />
+                            <span className='shareOptionContext'>Feelings</span>
+                        </div>
                     </div>
+                    <button className='shareButton'>Share</button>
                 </div>
             </div>
         </div>
